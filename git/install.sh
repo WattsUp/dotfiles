628a52f2caccb69d36a3f2119ad04125a0599871
Bradley Davis <me@bradleydavis.tech>
2023-10-13 10:40:25 -0700

    Removed updating submodules from remote

--- git/install.sh
+++ git/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Update submodules
-git submodule update --init --remote --recursive
+git submodule update --init --recursive
 
 # Install credential manager
 curl -L https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.3.2/gcm-linux_amd64.2.3.2.deb -o /var/tmp/gcm-linux.deb
