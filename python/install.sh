ca4c16ea1cc6dde789dfa1f2b02ceea53b2d2bfb
Bradley Davis <me@bradleydavis.tech>
2023-10-16 11:39:32 -0700

    Changed python install to 3.10

--- python/install.sh
+++ python/install.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-sudo apt install python3 python3-venv
+if ! sudo apt install python3.10 python3.10-venv -y; then
+	sudo apt install software-properties-common -y
+	sudo add-apt-repository ppa:deadsnakes/ppa
+	sudo apt install python3.10 python3.10-venv -y
+fi
