010ce7dd35ba420a531685ae5f8846da98e32f50
Bradley Davis <me@bradleydavis.tech>
2023-10-26 16:25:07 -0700

    Added zsh plugins

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,3 +2,5 @@
 sudo apt install zsh -y
 chsh -s /bin/zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
