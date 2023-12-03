ee205d5cadd6a0811287696f85a87567f256b4b4
Bradley Davis <me@bradleydavis.tech>
2023-12-02 18:39:05 -0800

    Added missing mkdir

--- git/install.sh
+++ git/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-sudo apt install tig
+sudo apt install tig pass
 
 # Update submodules
 git submodule update --init --recursive
@@ -9,3 +9,4 @@ git submodule update --init --recursive
 # Install credential manager
 curl -L https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.3.2/gcm-linux_amd64.2.3.2.deb -o /var/tmp/gcm-linux.deb
 sudo dpkg -i /var/tmp/gcm-linux.deb
+git-credential-manager configure
