d9d81f664e841f83e2bb62727d6e2336fa6a9370
Bradley Davis <me@bradleydavis.tech>
2023-12-02 19:11:23 -0800

    Added python dev to install

--- python/install.sh
+++ python/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-if ! sudo apt install python3.10 python3.10-venv -y; then
+if ! sudo apt install python3.10 python3.10-venv python3.10-dev -y; then
 	sudo apt install software-properties-common -y
 	sudo add-apt-repository ppa:deadsnakes/ppa
-	sudo apt install python3.10 python3.10-venv -y
+	sudo apt install python3.10 python3.10-venv python3.10-dev -y
 fi
