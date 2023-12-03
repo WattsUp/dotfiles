3fdd08fb03a60a95b67a258ce57469e142404672
Bradley Davis <me@bradleydavis.tech>
2023-12-02 18:09:58 -0800

    Fixed typo and overwriting

--- vim/install.sh
+++ vim/install.sh
@@ -3,7 +3,7 @@ set -e
 
 # Build and install neovim from source
 # ripgrep for searching in vim
-sudo apt install ninja-build gettext cmake unzip curl build-essentials ripgrep -y
+sudo apt install ninja-build gettext cmake unzip curl build-essential ripgrep -y
 cd /var/tmp
 curl -L https://github.com/neovim/neovim/archive/refs/tags/stable.tar.gz -o neovim.tar.gz
 tar -xzvf neovim.tar.gz
--- zsh/install.sh
+++ zsh/install.sh
@@ -3,6 +3,10 @@ set -e
 
 sudo apt install zsh -y
 chsh -s /bin/zsh
+rm -rf $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+rm -rf $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+rm -rf $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+rm -rf $HOME/.autoenv
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
