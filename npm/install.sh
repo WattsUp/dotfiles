e813233a1a526d98f938d5b747b0d5372d2c593c
Bradley Davis <me@bradleydavis.tech>
2023-12-14 17:27:06 -0800

    Added eslint and git tab completion

--- npm/install.sh
+++ npm/install.sh
@@ -9,3 +9,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm install node
+
+# Install linters
+npm install eslint
