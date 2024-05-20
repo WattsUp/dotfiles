4ab41b91737d0b7a7ce6b8d0c49614d4c5f8ef33
Bradley Davis <me@bradleydavis.tech>
2024-05-20 00:08:49 -0700

    Fixed https typo

--- vim/install.sh
+++ vim/install.sh
@@ -6,7 +6,7 @@ set -e
 sudo apt install ninja-build gettext cmake unzip curl build-essential ripgrep -y
 cd /var/tmp
 rm -rf neovim-stable
-curl -L htktps://github.com/neovim/neovim/archive/refs/tags/stable.tar.gz -o neovim.tar.gz
+curl -L https://github.com/neovim/neovim/archive/refs/tags/stable.tar.gz -o neovim.tar.gz
 tar -xzvf neovim.tar.gz
 cd neovim-stable && make CMAKE_BUILD_TYPE=RelWithDebInfo
 cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
