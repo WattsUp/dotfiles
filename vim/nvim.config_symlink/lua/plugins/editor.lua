91fd07fff3ed67833ad32b83b5823c39de16899f
Bradley Davis <me@bradleydavis.tech>
2024-06-25 11:01:47 -0700

    Fixed trouble label

--- vim/nvim.config_symlink/lua/plugins/editor.lua
+++ vim/nvim.config_symlink/lua/plugins/editor.lua
@@ -357,8 +357,9 @@ return {
       fold_closed = " ",
     },
     keys = {
-      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Document Diagnostics (Trouble)" },
-      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.bug=0<cr>", desc = "Workspace Diagnostics (Trouble)" },
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+
       { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
       { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
       {
