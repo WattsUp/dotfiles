72a8ea6f0c210f8619757ac29c3c5c416206e512
Bradley Davis <me@bradleydavis.tech>
2024-09-22 21:35:27 -0700

    Update installers when migrating to 24.04

--- python/install.sh
+++ python/install.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 set -e
 
-if ! sudo apt install python3.10 -y; then
-	sudo apt install software-properties-common -y
-	sudo add-apt-repository ppa:deadsnakes/ppa
-fi
-
-sudo apt install python3.10 python3.10-venv python3.10-dev python3-pip -y
-
-python3 -m pip install pre-commit yamllint
+sudo apt install python3.12 python3.12-venv python3.12-dev python3-pip -y
