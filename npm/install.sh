e052aabb97042cf482ff19ae9efeed4286db32d1
Bradley Davis <me@bradleydavis.tech>
2025-03-04 09:28:19 -0800

    Changed node install to global

--- npm/install.sh
+++ npm/install.sh
@@ -8,7 +8,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 # Run nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm install node
+nvm install node --global
 
 # Install linters
 npm install eslint --global
