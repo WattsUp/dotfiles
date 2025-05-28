d474a6ffd760e96de7b508c1b08982689e96d68e
Bradley Davis <me@bradleydavis.tech>
2025-05-28 10:32:08 -0700

    Updated autocmds

--- vim/nvim.config_symlink/lua/config/autocmds.lua
+++ vim/nvim.config_symlink/lua/config/autocmds.lua
@@ -1,17 +1,27 @@
+local function augroup(name)
+  return vim.api.nvim_create_augroup("bvim_" .. name, { clear = true })
+end
+
 -- Check if we need to reload the file when it changed
 vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
-  command = "checktime",
+  group = augroup("checktime"),
+  callback = function()
+    if vim.o.buftype ~= "nofile" then
+      vim.cmd("checktime")
+    end
+  end,
 })
 
 -- Go to last location when opening a buffer
 vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("last_loc"),
   callback = function(event)
     local exclude = { "gitcommit" }
     local buf = event.buf
-    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].last_loc then
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].bvim_last_loc then
       return
     end
-    vim.b[buf].last_loc = true
+    vim.b[buf].bvim_last_loc = true
     local mark = vim.api.nvim_buf_get_mark(buf, '"')
     local lcount = vim.api.nvim_buf_line_count(buf)
     if mark[1] > 0 and mark[1] <= lcount then
@@ -22,44 +32,51 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup("highlight_yank"),
   callback = function()
-    vim.highlight.on_yank({
-      higroup = "IncSearch",
-      timeout = 500,
-    })
+    (vim.hl or vim.highlight).on_yank()
   end,
 })
 
 -- Close some filetypes with <q>
 vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("close_with_q"),
   pattern = {
     "PlenaryTestPopup",
+    "checkhealth",
+    "dbout",
+    "gitsigns-blame",
+    "grug-far",
     "help",
     "lspinfo",
-    "man",
+    "neotest-output",
+    "neotest-output-panel",
+    "neotest-summary",
     "notify",
     "qf",
-    "query",
     "spectre_panel",
     "startuptime",
     "tsplayground",
-    "neotest-output",
-    "neotest-summary",
-    "neotest-output-panel",
-    "checkhealth",
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
-    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+    vim.schedule(function()
+      vim.keymap.set("n", "q", function()
+        vim.cmd("close")
+        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+      end, {
+        buffer = event.buf,
+        silent = true,
+        desc = "Quit buffer",
+      })
+    end)
   end,
 })
 
 -- Wrap and check spelling in text filetypes
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = {
-    "gitcommit",
-    "markdown",
-  },
+  group = augroup("wrap_spell"),
+  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.spell = true
@@ -68,17 +85,19 @@ vim.api.nvim_create_autocmd("FileType", {
 
 -- Auto create dir when saving a file
 vim.api.nvim_create_autocmd("BufWritePre", {
+  group = augroup("auto_create_dir"),
   callback = function(event)
-    if event.match:match("^%w%w+://") then
+    if event.match:match("^%w%w+:[\\/][\\/]") then
       return
     end
-    local file = vim.loop.fs_realpath(event.match) or event.match
+    local file = vim.uv.fs_realpath(event.match) or event.match
     vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
   end,
 })
 
 -- Auto set indent based on file type
 vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("indent_2"),
   pattern = { "lua", "markdown", "json", "toml" },
   callback = function()
     vim.opt_local.shiftwidth = 2
@@ -88,6 +107,7 @@ vim.api.nvim_create_autocmd("FileType", {
 
 -- YAML jinja templates get 2 indent
 vim.api.nvim_create_autocmd({ "BufEnter", "BufNewFile" }, {
+  group = augroup("indent_yaml_jinja_2"),
   pattern = "*yaml.jinja",
   callback = function()
     vim.opt_local.shiftwidth = 2
