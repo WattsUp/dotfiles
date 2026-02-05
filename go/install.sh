447c2043503f45776268b1c1bd1481e2345fdd1b
Bradley Davis <me@bradleydavis.tech>
2026-02-05 14:46:56 -0800

    Updated go version and added to nvim

--- go/install.sh
+++ go/install.sh
@@ -4,5 +4,5 @@ set -e
 sudo apt install curl -y
 sudo rm -rf /usr/local/go
 cd /var/tmp
-curl -L https://dl.google.com/go/go1.22.5.linux-amd64.tar.gz -o go.tar.gz
+curl -L https://dl.google.com/go/go1.22.12.linux-amd64.tar.gz -o go.tar.gz
 sudo tar -xvf go.tar.gz -C /usr/local
